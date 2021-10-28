769335a3a1d3041c77877ec114cefafb366acb26
Xoayer <xoayer@gmail.com>
2021-10-28 12:34:30 -0400

    LoadFunc finished

--- main.py
+++ main.py
@@ -7,15 +7,23 @@ Created on Wed Oct 27 14:55:37 2021
 import pandas as pd
 
 def main():
-    dataFolder = './data/'
-    data = loadCSV(dataFolder + 'wine.data')
-    print(data.iloc[:, 0])
+    
+    
+    data, target, feature_names, target_name, dataFile = loadCSV('./data/wine.data')
+
+
     
 
 #------------------supporting functions below------------------------#
 
 def loadCSV(dataFile):
     preData = pd.read_csv(dataFile)
-    labels = preData.iloc[:, 0]
+    target = preData.iloc[:, 0]
     data = preData.iloc[:,1::]
+    preNames = preData.columns
+    feature_names = [x for x in preNames] #to format it properly
+    target_name = feature_names[0]
+    feature_names = feature_names[1::]
+    
+    return data, target, feature_names, target_name, dataFile
     
